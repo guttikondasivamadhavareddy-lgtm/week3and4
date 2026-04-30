283025c0b750e6953eea99693c7d9606e4ca0ae5
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 19:05:05 +0530

    level 2 question 2 completed

--- week3and4.java
+++ week3and4.java
@@ -5,19 +5,17 @@ public class week3and4 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        System.out.print("Enter salary: ");
+        double salary = sc.nextDouble();
 
-        if (number >= 1) {
-            for (int i = 1; i <= number; i++) {
-                if (i % 2 == 0) {
-                    System.out.println(i + " is Even");
-                } else {
-                    System.out.println(i + " is Odd");
-                }
-            }
+        System.out.print("Enter years of service: ");
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
         } else {
-            System.out.println("Please enter a natural number");
+            System.out.println("No bonus");
         }
     }
 }

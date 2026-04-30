c9893cf366be5e2f3f02f4c44ecdf96198f9b1fd
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 19:30:41 +0530

    level 2 question 3 completed

--- week3and4.java
+++ week3and4.java
@@ -5,17 +5,11 @@ public class week3and4 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter salary: ");
-        double salary = sc.nextDouble();
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
 
-        System.out.print("Enter years of service: ");
-        int years = sc.nextInt();
-
-        if (years > 5) {
-            double bonus = salary * 0.05;
-            System.out.println("Bonus amount: " + bonus);
-        } else {
-            System.out.println("No bonus");
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
         }
     }
 }

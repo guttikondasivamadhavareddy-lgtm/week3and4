ace4c036117ea57d749522cd4c86094c652f6074
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 19:34:02 +0530

    level 3 question 1 completed

--- week3and4.java
+++ week3and4.java
@@ -5,11 +5,26 @@ public class week3and4 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
-        int number = sc.nextInt();
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
 
-        for (int i = 6; i <= 9; i++) {
-            System.out.println(number + " * " + i + " = " + (number * i));
+        if (year < 1582) {
+            System.out.println("Year should be >= 1582");
+        } 
+        else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println("Year is a Leap Year");
+                    } else {
+                        System.out.println("Year is not a Leap Year");
+                    }
+                } else {
+                    System.out.println("Year is a Leap Year");
+                }
+            } else {
+                System.out.println("Year is not a Leap Year");
+            }
         }
     }
 }
